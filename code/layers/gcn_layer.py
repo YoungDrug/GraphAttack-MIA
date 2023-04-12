b974bfe82e71757194bb7e76417093fd96b493e6
YoungDrug <102708237+YoungDrug@users.noreply.github.com>
2023-04-12 14:36:28 +0000

    Daily auto-update the BSL and OS repos

--- code/layers/gcn_layer.py
+++ code/layers/gcn_layer.py
@@ -20,4 +20,20 @@ reduce = fn.mean('m', 'h')
 class NodeApplyModule(nn.Module):
     # Update node feature h_v with (Wh_v+b)
     def __init__(self, in_dim, out_dim):
-      
+        super().__init__()
+        self.linear = nn.Linear(in_dim, out_dim)
+        
+    def forward(self, node):
+        h = self.linear(node.data['h'])
+        return {'h': h}
+
+class GCNLayer(nn.Module):
+    """
+        Param: [in_dim, out_dim]
+    """
+    def __init__(self, in_dim, out_dim, activation, dropout, batch_norm, residual=False, dgl_builtin=False):
+        super().__init__()
+        self.in_channels = in_dim
+        self.out_channels = out_dim
+        self.batch_norm = batch_norm
+        self.
