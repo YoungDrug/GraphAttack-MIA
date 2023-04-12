ea9c0583a6119129bddc87bdef00786d429b7372
YoungDrug <102708237+YoungDrug@users.noreply.github.com>
2023-04-12 09:46:43 +0000

    New package.

--- code/layers/gcn_layer.py
+++ code/layers/gcn_layer.py
@@ -3,4 +3,21 @@ import torch.nn as nn
 import torch.nn.functional as F
 
 import dgl
-import dgl.functio
+import dgl.function as fn
+from dgl.nn.pytorch import GraphConv
+
+"""
+    GCN: Graph Convolutional Networks
+    Thomas N. Kipf, Max Welling, Semi-Supervised Classification with Graph Convolutional Networks (ICLR 2017)
+    http://arxiv.org/abs/1609.02907
+"""
+    
+# Sends a message of node feature h
+# Equivalent to => return {'m': edges.src['h']}
+msg = fn.copy_src(src='h', out='m')
+reduce = fn.mean('m', 'h')
+
+class NodeApplyModule(nn.Module):
+    # Update node feature h_v with (Wh_v+b)
+    def __init__(self, in_dim, out_dim):
+      
