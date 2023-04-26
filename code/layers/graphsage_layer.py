0c7f375c106fa6f2f22ff8462ed6b59d7fc8cf69
YoungDrug <102708237+YoungDrug@users.noreply.github.com>
2023-04-26 14:43:07 +0000

    Update bitData.html

--- code/layers/graphsage_layer.py
+++ code/layers/graphsage_layer.py
@@ -8,4 +8,20 @@ from dgl.nn.pytorch import SAGEConv
 """
     GraphSAGE: 
     William L. Hamilton, Rex Ying, Jure Leskovec, Inductive Representation Learning on Large Graphs (NeurIPS 2017)
-    https://cs.stanford.edu/people/jure/
+    https://cs.stanford.edu/people/jure/pubs/graphsage-nips17.pdf
+"""
+
+class GraphSageLayer(nn.Module):
+
+    def __init__(self, in_feats, out_feats, activation, dropout,
+                 aggregator_type, batch_norm, residual=False, 
+                 bias=True, dgl_builtin=False):
+        super().__init__()
+        self.in_channels = in_feats
+        self.out_channels = out_feats
+        self.aggregator_type = aggregator_type
+        self.batch_norm = batch_norm
+        self.residual = residual
+        self.dgl_builtin = dgl_builtin
+        
+        if in_feats != out
