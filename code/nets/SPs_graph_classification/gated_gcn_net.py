9a8ab39d0b99d55934781c7c568cdaf7d270440a
YoungDrug <102708237+YoungDrug@users.noreply.github.com>
2023-06-06 14:35:35 +0000

    Regenerated gemspec for version 0.2.3

--- code/nets/SPs_graph_classification/gated_gcn_net.py
+++ code/nets/SPs_graph_classification/gated_gcn_net.py
@@ -1,3 +1,4 @@
+
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
@@ -5,43 +6,48 @@ import torch.nn.functional as F
 import dgl
 
 """
-    GAT: Graph Attention Network
-    Graph Attention Networks (Veličković et al., ICLR 2018)
-    https://arxiv.org/abs/1710.10903
+    ResGatedGCN: Residual Gated Graph ConvNets
+    An Experimental Study of Neural Networks for Variable Graphs (Xavier Bresson and Thomas Laurent, ICLR 2018)
+    https://arxiv.org/pdf/1711.07553v2.pdf
 """
-from layers.gat_layer import GATLayer
+from layers.gated_gcn_layer import GatedGCNLayer
 from layers.mlp_readout_layer import MLPReadout
 
-class GATNet(nn.Module):
+class GatedGCNNet(nn.Module):
+    
     def __init__(self, net_params):
         super().__init__()
         in_dim = net_params['in_dim']
+        in_dim_edge = net_params['in_dim_edge']
         hidden_dim = net_params['hidden_dim']
         out_dim = net_params['out_dim']
         n_classes = net_params['n_classes']
-        num_heads = net_params['n_heads']
-        in_feat_dropout = net_params['in_feat_dropout']
         dropout = net_params['dropout']
         n_layers = net_params['L']
         self.readout = net_params['readout']
         self.batch_norm = net_params['batch_norm']
         self.residual = net_params['residual']
-        self.dropout = dropout
+        self.edge_feat = net_params['edge_feat']
+        self.device = net_params['device']
         
-        self.embedding_h = nn.Linear(in_dim, hidden_dim * num_heads)
-        
-        self.in_feat_dropout = nn.Dropout(in_feat_dropout)
-        
-        self.layers = nn.ModuleList([GATLayer(hidden_dim * num_heads, hidden_dim, num_heads,
-                                              dropout, self.batch_norm, self.residual) for _ in range(n_layers-1)])
-        self.layers.append(GATLayer(hidden_dim * num_heads, out_dim, 1, dropout, self.batch_norm, self.residual))
+        self.embedding_h = nn.Linear(in_dim, hidden_dim)
+        self.embedding_e = nn.Linear(in_dim_edge, hidden_dim)
+        self.layers = nn.ModuleList([ GatedGCNLayer(hidden_dim, hidden_dim, dropout,
+                                                    self.batch_norm, self.residual) for _ in range(n_layers-1) ]) 
+        self.layers.append(GatedGCNLayer(hidden_dim, out_dim, dropout, self.batch_norm, self.residual))
         self.MLP_layer = MLPReadout(out_dim, n_classes)
         
     def forward(self, g, h, e):
+
+        # input embedding
         h = self.embedding_h(h)
-        h = self.in_feat_dropout(h)
+        if not self.edge_feat: # edge feature set to 1
+            e = torch.ones_like(e).to(self.device)
+        e = self.embedding_e(e)
+        
+        # convnets
         for conv in self.layers:
-            h = conv(g, h)
+            h, e = conv(g, h, e)
         g.ndata['h'] = h
         
         if self.readout == "sum":
@@ -54,7 +60,7 @@ class GATNet(nn.Module):
             hg = dgl.mean_nodes(g, 'h')  # default readout is mean nodes
             
         return self.MLP_layer(hg)
-    
+        
     def loss(self, pred, label):
         criterion = nn.CrossEntropyLoss()
         loss = criterion(pred, label)
---
+++ code/nets/SPs_graph_classification/gcn_net.py
@@ -0,0 +1,10 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+import dgl
+
+"""
+    GCN: Graph Convolutional Networks
+    Thomas N. Kipf, Max Welling, Semi-Supervised Classification with Graph Convolutional Networks (ICLR 2017)
+    http://arx
