5c37af0e3b56e0f94718ad3e4eaae68025821b54
YoungDrug <102708237+YoungDrug@users.noreply.github.com>
2023-06-07 16:29:30 +0000

    Update sample.txt

--- code/nets/SPs_graph_classification/gin_net.py
+++ code/nets/SPs_graph_classification/gin_net.py
@@ -6,3 +6,19 @@ import dgl
 from dgl.nn.pytorch.glob import SumPooling, AvgPooling, MaxPooling
 
 """
+    GIN: Graph Isomorphism Networks
+    HOW POWERFUL ARE GRAPH NEURAL NETWORKS? (Keyulu Xu, Weihua Hu, Jure Leskovec and Stefanie Jegelka, ICLR 2019)
+    https://arxiv.org/pdf/1810.00826.pdf
+"""
+
+from layers.gin_layer import GINLayer, ApplyNodeFunc, MLP
+
+class GINNet(nn.Module):
+    
+    def __init__(self, net_params):
+        super().__init__()
+        in_dim = net_params['in_dim']
+        hidden_dim = net_params['hidden_dim']
+        n_classes = net_params['n_classes']
+        dropout = net_params['dropout']
+        self.n_lay
