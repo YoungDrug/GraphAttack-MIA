ffc3207e69ce64e9d1ac2643f188a409d30dbea3
YoungDrug <102708237+YoungDrug@users.noreply.github.com>
2023-07-15 14:38:00 +0000

    Italian Translation

--- code/nets/SPs_graph_classification/mo_net.py
+++ code/nets/SPs_graph_classification/mo_net.py
@@ -9,4 +9,20 @@ import numpy as np
 """
     GMM: Gaussian Mixture Model Convolution layer
     Geometric Deep Learning on Graphs and Manifolds using Mixture Model CNNs (Federico Monti et al., CVPR 2017)
-    https://arxiv.org/pdf/161
+    https://arxiv.org/pdf/1611.08402.pdf
+"""
+
+from layers.gmm_layer import GMMLayer
+from layers.mlp_readout_layer import MLPReadout
+
+class MoNet(nn.Module):
+    def __init__(self, net_params):
+        super().__init__()
+        self.name = 'MoNet'
+        in_dim = net_params['in_dim']
+        hidden_dim = net_params['hidden_dim']
+        out_dim = net_params['out_dim']
+        kernel = net_params['kernel']                       # for MoNet
+        dim = net_params['pseudo_dim_MoNet']                # for MoNet
+        n_classes = net_params['n_classes']
+        dropout = net_para
