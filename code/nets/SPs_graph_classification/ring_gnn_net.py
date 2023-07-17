c139c5b1b84b3e185ad3f117b8fb8d9f40a7377a
YoungDrug <102708237+YoungDrug@users.noreply.github.com>
2023-07-17 06:00:39 +0000

    fix graph

--- code/nets/SPs_graph_classification/ring_gnn_net.py
+++ code/nets/SPs_graph_classification/ring_gnn_net.py
@@ -7,4 +7,16 @@ import time
 
 """
     Ring-GNN
-    On
+    On the equivalence between graph isomorphism testing and function approximation with GNNs (Chen et al, 2019)
+    https://arxiv.org/pdf/1905.12560v1.pdf
+"""
+from layers.ring_gnn_equiv_layer import RingGNNEquivLayer
+from layers.mlp_readout_layer import MLPReadout
+
+class RingGNNNet(nn.Module):
+    def __init__(self, net_params):
+        super().__init__()
+        self.in_dim_node = net_params['in_dim']
+        avg_node_num = net_params['avg_node_num'] 
+        radius = net_params['radius'] 
+      
