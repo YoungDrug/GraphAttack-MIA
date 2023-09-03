fb91be3be86d39578993cfefb8c64a0ee74bc66b
YoungDrug <102708237+YoungDrug@users.noreply.github.com>
2023-09-03 07:02:42 +0000

    Add .gitignore and .gitattributes.

--- code/nets/TUs_graph_classification/gated_gcn_net.py
+++ code/nets/TUs_graph_classification/gated_gcn_net.py
@@ -19,4 +19,14 @@ class GatedGCNNet(nn.Module):
         in_dim = net_params['in_dim']
         hidden_dim = net_params['hidden_dim']
         out_dim = net_params['out_dim']
-        n_classes = net_params['n
+        n_classes = net_params['n_classes']
+        dropout = net_params['dropout']
+        n_layers = net_params['L']
+        self.readout = net_params['readout']
+        self.batch_norm = net_params['batch_norm']
+        self.residual = net_params['residual']
+        
+        self.embedding_h = nn.Linear(in_dim, hidden_dim)
+        self.embedding_e = nn.Linear(in_dim, hidden_dim)
+        self.layers = nn.ModuleList([ GatedGCNLayer(hidden_dim, hidden_dim, dropout,
+                                                       self.batch_n
