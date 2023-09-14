bdd277cfb5b6d66b3e751088ef031e73e95289f3
YoungDrug <102708237+YoungDrug@users.noreply.github.com>
2023-09-14 07:13:15 +0000

    improve docstring

--- code/nets/TUs_graph_classification/gated_gcn_net.py
+++ code/nets/TUs_graph_classification/gated_gcn_net.py
@@ -42,4 +42,21 @@ class GatedGCNNet(nn.Module):
             h, e = conv(g, h, e)
         g.ndata['h'] = h
         
-        if self.readout ==
+        if self.readout == "sum":
+            hg = dgl.sum_nodes(g, 'h')
+        elif self.readout == "max":
+            hg = dgl.max_nodes(g, 'h')
+        elif self.readout == "mean":
+            hg = dgl.mean_nodes(g, 'h')
+        else:
+            hg = dgl.mean_nodes(g, 'h')  # default readout is mean nodes
+            
+        return self.MLP_layer(hg)
+        
+    def loss(self, pred, label):
+        criterion = nn.CrossEntropyLoss()
+        loss = criterion(pred, label)
+        return loss
+    
+
+        
