064e700a7d03bdd6175d1ceeb6fd2734049b6a46
YoungDrug <102708237+YoungDrug@users.noreply.github.com>
2023-09-24 11:08:05 +0000

    net: add validation for the socket syscall protocol argument

--- code/nets/TUs_graph_classification/load_net.py
+++ code/nets/TUs_graph_classification/load_net.py
@@ -5,4 +5,18 @@
 
 from nets.TUs_graph_classification.gated_gcn_net import GatedGCNNet
 from nets.TUs_graph_classification.gcn_net import GCNNet
-from nets.TUs_graph_classification.
+from nets.TUs_graph_classification.gat_net import GATNet
+from nets.TUs_graph_classification.graphsage_net import GraphSageNet
+from nets.TUs_graph_classification.gin_net import GINNet
+from nets.TUs_graph_classification.mo_net import MoNet as MoNet_
+from nets.TUs_graph_classification.mlp_net import MLPNet
+from nets.TUs_graph_classification.ring_gnn_net import RingGNNNet
+from nets.TUs_graph_classification.three_wl_gnn_net import ThreeWLGNNNet
+
+
+def GatedGCN(net_params):
+    return GatedGCNNet(net_params)
+
+
+def GCN(net_params):
+    r
