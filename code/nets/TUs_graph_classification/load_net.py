11a47ef517da290d38264598c41f87ccbd01bfce
YoungDrug <102708237+YoungDrug@users.noreply.github.com>
2023-09-25 10:12:06 +0000

    file name changed

--- code/nets/TUs_graph_classification/load_net.py
+++ code/nets/TUs_graph_classification/load_net.py
@@ -19,4 +19,36 @@ def GatedGCN(net_params):
 
 
 def GCN(net_params):
-    r
+    return GCNNet(net_params)
+
+
+def GAT(net_params):
+    return GATNet(net_params)
+
+
+def GraphSage(net_params):
+    return GraphSageNet(net_params)
+
+
+def GIN(net_params):
+    return GINNet(net_params)
+
+
+def MoNet(net_params):
+    return MoNet_(net_params)
+
+
+def MLP(net_params):
+    return MLPNet(net_params)
+
+
+def RingGNN(net_params):
+    return RingGNNNet(net_params)
+
+
+def ThreeWLGNN(net_params):
+    return ThreeWLGNNNet(net_params)
+
+
+def gnn_model(MODEL_NAME, net_params):
+ 
