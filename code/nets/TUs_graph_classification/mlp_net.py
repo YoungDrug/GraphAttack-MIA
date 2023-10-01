2292718a8a055ae8afeed40f0b46f78c588ed2b6
YoungDrug <102708237+YoungDrug@users.noreply.github.com>
2023-10-01 06:38:37 +0000

    see ChangeLog file

--- code/nets/TUs_graph_classification/mlp_net.py
+++ code/nets/TUs_graph_classification/mlp_net.py
@@ -8,4 +8,20 @@ from layers.mlp_readout_layer import MLPReadout
 
 class MLPNet(nn.Module):
     def __init__(self, net_params):
-        super().__in
+        super().__init__()
+        in_dim = net_params['in_dim']
+        hidden_dim = net_params['hidden_dim']
+        n_classes = net_params['n_classes']
+        in_feat_dropout = net_params['in_feat_dropout']
+        dropout = net_params['dropout']
+        n_layers = net_params['L']
+        self.gated = net_params['gated']
+        
+        self.in_feat_dropout = nn.Dropout(in_feat_dropout)
+        
+        feat_mlp_modules = [
+            nn.Linear(in_dim, hidden_dim, bias=True),
+            nn.ReLU(),
+            nn.Dropout(dropout),
+        ]
+  
