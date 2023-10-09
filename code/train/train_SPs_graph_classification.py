5227f77cf187403f3990ec72e0a9e789c47e2a61
YoungDrug <102708237+YoungDrug@users.noreply.github.com>
2023-10-09 18:07:58 +0000

    reconsitution

--- code/train/train_SPs_graph_classification.py
+++ code/train/train_SPs_graph_classification.py
@@ -1,2 +1,26 @@
 """
-  
+    Utility functions for training one epoch 
+    and evaluating one epoch
+"""
+import os
+import pickle
+
+import dgl
+import torch
+import torch.nn as nn
+import math
+import numpy as np
+import torch.nn.functional as F
+
+from train.metrics import accuracy_MNIST_CIFAR as accuracy
+
+"""
+    For GCNs
+"""
+def train_epoch_sparse(model, optimizer, device, data_loader, epoch):
+    model.train()
+    epoch_loss = 0
+    epoch_train_acc = 0
+    nb_data = 0
+    gpu_mem = 0
+    for iter, (batch_graphs, batch_labels
