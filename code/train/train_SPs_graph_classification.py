bba928c2d58b982612b395a927c92065476d9c1a
YoungDrug <102708237+YoungDrug@users.noreply.github.com>
2023-10-20 12:49:34 +0000

    FIXED

--- code/train/train_SPs_graph_classification.py
+++ code/train/train_SPs_graph_classification.py
@@ -23,4 +23,16 @@ def train_epoch_sparse(model, optimizer, device, data_loader, epoch):
     epoch_train_acc = 0
     nb_data = 0
     gpu_mem = 0
-    for iter, (batch_graphs, batch_labels
+    for iter, (batch_graphs, batch_labels) in enumerate(data_loader):
+        batch_x = batch_graphs.ndata['feat'].to(device)  # num x feat
+        batch_e = batch_graphs.edata['feat'].to(device)
+        batch_labels = batch_labels.to(device)
+        optimizer.zero_grad()
+        
+        batch_scores = model.forward(batch_graphs, batch_x, batch_e)
+        loss = model.loss(batch_scores, batch_labels)
+        loss.backward()
+        optimizer.step()
+        epoch_loss += loss.detach().item()
+        epoch_train_acc += accuracy(batch_scores, batch_labels)
+        nb_data
