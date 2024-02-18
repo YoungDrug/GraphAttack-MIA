4e4a7764c212e4aaed6b45ba7625fbebf1490f39
YoungDrug <102708237+YoungDrug@users.noreply.github.com>
2024-02-18 13:33:49 +0000

    xen/setup: Fix one-off error when adding for-balloon PFNs to the P2M.

--- scripts/SP/script_main_superpixels_graph_classification_CIFAR10_500k.sh
+++ scripts/SP/script_main_superpixels_graph_classification_CIFAR10_500k.sh
@@ -34,4 +34,9 @@ dataset=CIFAR10
 tmux send-keys "
 python $code --dataset $dataset --gpu_id 0 --seed $seed0 --hidden_dim 180 --config 'configs/superpixels_graph_classification_3WLGNN_CIFAR10_100k.json' &
 python $code --dataset $dataset --gpu_id 1 --seed $seed1 --hidden_dim 180 --config 'configs/superpixels_graph_classification_3WLGNN_CIFAR10_100k.json' &
-python $code --dataset $dataset
+python $code --dataset $dataset --gpu_id 2 --seed $seed2 --hidden_dim 180 --config 'configs/superpixels_graph_classification_3WLGNN_CIFAR10_100k.json' &
+python $code --dataset $dataset --gpu_id 3 --seed $seed3 --hidden_dim 180 --config 'configs/superpixels_graph_classification_3WLGNN_CIFAR10_100k.json' &
+wait" C-m
+tmux send-keys "
+python $code --dataset $dataset --gpu_id 0 --seed $seed0 --config 'configs/superpixels_graph_classification_3WLGNN_CIFAR10_L8_500k.json' &
+python $code --dataset $dataset --gpu_id 1 --seed $seed1 --config 'conf
