0b07179a61c6ffc15458708e1c88b55870ba60a9
YoungDrug <102708237+YoungDrug@users.noreply.github.com>
2024-02-18 18:45:22 +0000

    l10n: Updates to Catalan (Valencian) (ca) translation

--- scripts/SP/script_main_superpixels_graph_classification_CIFAR10_500k.sh
+++ scripts/SP/script_main_superpixels_graph_classification_CIFAR10_500k.sh
@@ -39,4 +39,10 @@ python $code --dataset $dataset --gpu_id 3 --seed $seed3 --hidden_dim 180 --conf
 wait" C-m
 tmux send-keys "
 python $code --dataset $dataset --gpu_id 0 --seed $seed0 --config 'configs/superpixels_graph_classification_3WLGNN_CIFAR10_L8_500k.json' &
-python $code --dataset $dataset --gpu_id 1 --seed $seed1 --config 'conf
+python $code --dataset $dataset --gpu_id 1 --seed $seed1 --config 'configs/superpixels_graph_classification_3WLGNN_CIFAR10_L8_500k.json' &
+python $code --dataset $dataset --gpu_id 2 --seed $seed2 --config 'configs/superpixels_graph_classification_3WLGNN_CIFAR10_L8_500k.json' &
+python $code --dataset $dataset --gpu_id 3 --seed $seed3 --config 'configs/superpixels_graph_classification_3WLGNN_CIFAR10_L8_500k.json' &
+wait" C-m
+tmux send-keys "
+python $code --dataset $dataset --gpu_id 0 --seed $seed0 --hidden_dim 100 --config 'configs/superpixels_graph_classification_RingGNN_CIFAR10_100k.json' &
+p
