a8959a6f77ca0cbdf39f57f531e87ef78da15659
YoungDrug <102708237+YoungDrug@users.noreply.github.com>
2024-02-18 06:20:09 +0000

    [Security] Bump ini from 1.3.5 to 1.3.7

--- scripts/SP/script_main_superpixels_graph_classification_CIFAR10_500k.sh
+++ scripts/SP/script_main_superpixels_graph_classification_CIFAR10_500k.sh
@@ -27,4 +27,11 @@ seed0=41
 seed1=95
 seed2=12
 seed3=35
-code=main_superpixels
+code=main_superpixels_graph_classification.py 
+tmux new -s benchmark_CIFAR10 -d
+tmux send-keys "source activate benchmark_gnn" C-m
+dataset=CIFAR10
+tmux send-keys "
+python $code --dataset $dataset --gpu_id 0 --seed $seed0 --hidden_dim 180 --config 'configs/superpixels_graph_classification_3WLGNN_CIFAR10_100k.json' &
+python $code --dataset $dataset --gpu_id 1 --seed $seed1 --hidden_dim 180 --config 'configs/superpixels_graph_classification_3WLGNN_CIFAR10_100k.json' &
+python $code --dataset $dataset
